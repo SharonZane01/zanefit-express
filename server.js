130bfd49391001c7bea35176d24d412e341aa2b1
Sharon S <sharonzane795@gmail.com>
2025-04-11 18:40:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,3 +17,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
