e0117f6ea5f82e4572b210540cc28658b8d2387f
Sharon S <sharonzane795@gmail.com>
2025-04-14 18:11:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: ['https://zane-fit.vercel.app', 'https://zanefit.netlify.app']
+     credentials: true
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type']
 }));
