be05651c5398c6b396e9d2cca6d6c1a921a68538
Sharon S <sharonzane795@gmail.com>
2025-04-15 17:36:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const WorkoutRoutes = require('./routes/WorkoutRoutes');
+const NutritionRoutes = require('./routes/NutritionRoutes');
+const ProgressRoutes = require('./routes/ProgressRoutes');
+const EquipmentRoutes = require('./routes/EquipmentRoutes');
 
 const app = express();
 
@@ -15,6 +18,10 @@ app.use(express.json());
 
 // Routes
 app.use('/api/workout', WorkoutRoutes);
+app.use('/api/nutrition', NutritionRoutes);
+app.use('/api/progress', ProgressRoutes);
+app.use('/api/equipment', EquipmentRoutes);
+
 
 // Test route
 app.get('/', (req, res) => {
